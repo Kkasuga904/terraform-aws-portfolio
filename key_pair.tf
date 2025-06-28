06aa40de61ea0b9fde7cd20c5f922bc71def3a63
Kasuga46 <over9131120@gmail.com>
2025-06-28 22:34:00 +0900

    Fix: Apply terraform fmt formatting

--- key_pair.tf
+++ key_pair.tf
@@ -1,7 +1,7 @@
 
- resource "aws_key_pair" "deployer" {
- key_name = "deployer-key"
- public_key = file("${path.module}/../../key/koukaikagi.pub")
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("${path.module}/../../key/koukaikagi.pub")
 }
 
 
--- locals.tf
+++ locals.tf
@@ -1,29 +1,29 @@
 locals {
- target_date = "2025-05-11"
- region = var.aws_region
+  target_date = "2025-05-11"
+  region      = var.aws_region
 
- name = "example-instance"
- id = 123
- tags = {
-  Environment = "dev"
-  Region = local.region
- }
-
-
-ips = ["10.0.0.1","10.0.0.2"]
-enable = true
- 
+  name = "example-instance"
+  id   = 123
+  tags = {
+    Environment = "dev"
+    Region      = local.region
+  }
 
 
-output_data = jsonencode({
-  instance_name = local.name
-  instance_id = local.id
-  resource_tags = local.tags
-  ip_addresses = local.ips
-  is_enabled = local.enable
-  target = local.target_date
-  aws_region = local.region
-})
+  ips    = ["10.0.0.1", "10.0.0.2"]
+  enable = true
+
+
+
+  output_data = jsonencode({
+    instance_name = local.name
+    instance_id   = local.id
+    resource_tags = local.tags
+    ip_addresses  = local.ips
+    is_enabled    = local.enable
+    target        = local.target_date
+    aws_region    = local.region
+  })
 
 
   monitored_instance_ids = [
