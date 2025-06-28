f91e9a3fbf63db059fcf6419b1db1fae8548a758
Kasuga46 <over9131120@gmail.com>
2025-06-28 22:34:00 +0900

    Fix: Apply terraform fmt formatting

--- main.tf
+++ main.tf
@@ -3,19 +3,19 @@ provider "aws" {
 }
 
 module "ec2_instances" {
-  source = "./modules/ec2"
-  instances = var.ec2_instances
+  source            = "./modules/ec2"
+  instances         = var.ec2_instances
   common_tags       = var.common_tags
   enable_monitoring = var.enable_monitoring
 }
 
 output "instance_public_ips" {
-  value = module.ec2_instances.instance_public_ips
+  value       = module.ec2_instances.instance_public_ips
   description = "Created EC2 instance IPs"
 }
 
 module "s3_bucket" {
-  source = "./modules/s3" # S3 モジュールのパス
+  source      = "./modules/s3" # S3 モジュールのパス
   bucket_name = var.s3_bucket_name
   common_tags = var.common_tags
   # その他の S3 バケットの設定
@@ -26,15 +26,15 @@ module "network" {
 }
 
 module "units" {
-  source = "./modules/units"
-  s3_bucket_name  = module.s3_bucket.bucket_name
+  source         = "./modules/units"
+  s3_bucket_name = module.s3_bucket.bucket_name
 }
 
 module "security_group" {
   source = "./modules/security_group"
 
-  vpc_id       = module.network.vpc_id
-  common_tags  = var.common_tags
+  vpc_id      = module.network.vpc_id
+  common_tags = var.common_tags
 }
 
 module "cloudwatch" {
