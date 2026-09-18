97b47269e01444d824b825fcbc6bfa85abaadd0b
Kairi Muroki <130277788+Kkasuga904@users.noreply.github.com>
2026-09-18 17:22:00 +0900

    security: remove local configuration from version control

--- key_pair.tf
+++ key_pair.tf
@@ -1,8 +1,13 @@
 
- resource "aws_key_pair" "deployer" {
- key_name = "deployer-key"
- public_key = file("C:/Users/Owner/Downloads/koukaikagi.pub")
+variable "deployer_public_key" {
+  description = "OpenSSH public key used for the deployer key pair"
+  type        = string
+  sensitive   = true
+}
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = var.deployer_public_key
 }
 
 
